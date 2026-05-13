77f6bd158f36b56b09dbfe9f1bb9767228326e67
JirkaKrajicek <j.krajicek6830@gmail.com>
2026-05-13 15:45:57 +0200

    demo on coffee maker

--- src/main.rs
+++ src/main.rs
@@ -3,4 +3,18 @@
 
 fn main() {
     println!("Hello, world!");
+
+    let point = Point::new(1, 2);   
+    point.sum(3);
+
+    let cappuccino = Cappuccino::default();
+    // Result<FlatWhite, String>
+    let flat_white = match FlatWhite::try_from(cappuccino) {
+        Some(coffee) => coffee,
+        None
+    }
+
+    if let Some(coffee) = match FlatWhite::try_from(cappuccino) {
+        println!("Error converting Cappuccino to FlatWhite: {}", err);
+    }
 }
